96d575c2c14146aaab25ea4d2d04c2bff6a15947
stepanvalic <valicekstepan8@seznam.cz>
2025-04-30 22:29:49 +0200

    new status messages --restart

--- cogs/bot_activity.py
+++ cogs/bot_activity.py
@@ -3,12 +3,14 @@ from discord.ext import commands, tasks
 from utils import config
 
 GUILD_ID = config.get_int('GUILD_ID')
-ACTIVITY_BASE_TEXT = config.get('ACTIVITY_BASE_TEXT', 'Sleduji')
-ACTIVITY_FORMAT_TEXT = config.get('ACTIVITY_FORMAT_TEXT', '{count} darebáků')
+ACTIVITY_TEXT_1 = config.get('ACTIVITY_TEXT_1', '{count} kočičkářů')
+ACTIVITY_TEXT_2 = config.get('ACTIVITY_TEXT_2', '{count} darebáků')
+ACTIVITY_TEXT_3 = config.get('ACTIVITY_TEXT_3', '{count} obránců cat army')
 
 class BotActivity(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.current_activity_index = 0
         self.change_status.start()
 
     def cog_unload(self):
@@ -18,7 +20,14 @@ class BotActivity(commands.Cog):
     async def change_status(self):
         guild = self.bot.get_guild(GUILD_ID)
         member_count = guild.member_count if guild else 0
-        status = f"{ACTIVITY_BASE_TEXT} {ACTIVITY_FORMAT_TEXT.format(count=member_count)}"
+
+        # Rotace mezi třemi texty aktivity
+        activity_texts = [ACTIVITY_TEXT_1, ACTIVITY_TEXT_2, ACTIVITY_TEXT_3]
+        current_text = activity_texts[self.current_activity_index]
+        status = current_text.format(count=member_count)
+
+        # Přepnutí na další text pro příští volání
+        self.current_activity_index = (self.current_activity_index + 1) % len(activity_texts)
 
         await self.bot.change_presence(
             activity=discord.Activity(
